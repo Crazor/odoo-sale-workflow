03777b7dcc5d9123cae5b849f62aa5ed1f16878b
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-12-20 08:52:00 -0600

    [IMP] sale_product_returnable

--- sale_product_returnable/__manifest__.py
+++ sale_product_returnable/__manifest__.py
@@ -1,21 +1,22 @@
 # Copyright (C) 2019 Open Source Integrators
 # Copyright (C) 2019 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
-
 {
-    'name': 'Sale Product Retunable',
+    'name': 'Sale Product Returnable',
+    'summary': "Get returnable products from your customers",
     'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'category': 'Sales',
-    'author': 'Open Source Integrators, Odoo Community Association (OCA)',
+    'author': 'Serpent Consulting Services, '
+              'Open Source Integrators, '
+              'Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/sale-workflow',
     'development_status': 'Production/Stable',
-    'summary': """This module allows you to set some product as returnable
-                and create a reception when the sales order is confirmed.""",
     'depends': ['sale_stock'],
     'maintainers': ['max3903'],
     'data': [
         'views/product_template_view.xml',
     ],
-    'installable': True,
+    'appplication': False,
+    'maintainers': ['max3903'],
 }
